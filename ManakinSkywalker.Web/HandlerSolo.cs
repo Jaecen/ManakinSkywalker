abc19b407430144f74e37cbdc5b42dfe20cd2bcd
Jason Addington <jason@addington.com>
2013-03-27 17:59:43 -0700

    Initial implementation. Supports verb matching and regex on URL.

---
+++ ManakinSkywalker.Web/HandlerSolo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace ManakinSkywalker.Web
+{
+	public class HandlerSolo : IHttpHandler
+	{
+		public bool IsReusable
+		{ get { return true; } }
+
+		public void ProcessRequest(HttpContext context)
+		{
+			R2Data dataContext = new R2Data();
+			dataContext.Database.CreateIfNotExists();
+
+			var response = dataContext.Mappings
+				.Where(m => m.Verb == context.Request.HttpMethod)
+				.ToArray()
+				.Where(m => new Regex(m.Url).IsMatch(context.Request.AppRelativeCurrentExecutionFilePath))
+				.Select(m => m.Response)
+				.FirstOrDefault();
+
+			if(response == null)
+			{
+				context.Response.StatusCode = 404;
+				context.Response.End();
+			}
+
+			context.Response.StatusCode = 200;
+			context.Response.Write(response);
+			context.Response.End();
+		}
+	}
+}
---
+++ ManakinSkywalker.Web/R2Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ManakinSkywalker.Web
+{
+	public class R2Data : DbContext
+	{
+		public virtual DbSet<Mapping> Mappings { get; set; }
+	}
+
+	public class Mapping
+	{
+		public virtual int Id { get; set; }
+		public virtual string Verb { get; set; }
+		public virtual string Url { get; set; }
+		public virtual string Response { get; set; }
+	}
+}
